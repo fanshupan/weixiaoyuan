e55c10ef8ea8d2c96f60bf89602bd10b56de12d4
方朔 <fs.wm@alibaba-inc.com>
2014-10-18 10:33:27 +0800

    1.init commting

---
+++ 微校园/微校园/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  微校园
+//
+//  Created by wuming on 14/10/18.
+//  Copyright (c) 2014年 wu ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 微校园/微校园/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  微校园
+//
+//  Created by wuming on 14/10/18.
+//  Copyright (c) 2014年 wu ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
