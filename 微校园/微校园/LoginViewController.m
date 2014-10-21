aad7dd77f29b2bfc2fe79866d47ad4d3cae693df
fanshupan <hanshupan@shrek.local>
2014-10-21 15:26:31 +0800

    键盘

--- 微校园/微校园/LoginViewController.m
+++ 微校园/微校园/LoginViewController.m
@@ -26,16 +26,24 @@
     self.emailField=[[UITextField alloc]initWithFrame:CGRectMake(10, 100, self.view.frame.size.width-20, 30)];
     self.emailField.placeholder=@"Email";
     self.emailField.backgroundColor=[UIColor whiteColor];
-    self.emailField.clearButtonMode = YES;
-    self.emailField.delegate = self;
+    self.emailField.clearButtonMode = UITextFieldViewModeWhileEditing;
     [self.view addSubview:self.emailField];
     
+    self.emailField.delegate = self;
+    
+    
     self.passwordField=[[UITextField alloc]initWithFrame:CGRectMake(10, 135, self.view.frame.size.width-20, 30)];
-        self.passwordField.placeholder=@"Password";
-        self.passwordField.backgroundColor=[UIColor whiteColor];
-        self.passwordField.secureTextEntry = YES;
+    self.passwordField.placeholder=@"Password";
+    self.passwordField.backgroundColor=[UIColor whiteColor];
+    self.passwordField.clearButtonMode=UITextFieldViewModeWhileEditing;
+    self.passwordField.secureTextEntry = YES;
    // passwordField.delegate= self;
     [self.view addSubview:self.passwordField];
+    
+    
+    UITextField *testField=[[UITextField alloc]initWithFrame:CGRectMake(10, 170, 100, 25)];
+    testField.backgroundColor=[UIColor redColor];
+    [self.view addSubview:testField];
     
     UIButton *loginButton=[UIButton buttonWithType:UIButtonTypeCustom];
     loginButton.backgroundColor=[UIColor colorWithRed:141/255.0f green:203/255.0f blue:31/255.0f alpha:1.0];
@@ -44,18 +52,18 @@
     [loginButton setTitle:@"Login" forState:UIControlStateNormal];
     loginButton.titleLabel.font=[UIFont systemFontOfSize:18];
     [loginButton setTitle:@"登录" forState:UIControlStateHighlighted];
+   
     UIButton *registerButton=[UIButton buttonWithType:UIButtonTypeCustom];
-    
     [registerButton setBackgroundColor:[UIColor colorWithRed:0/255.0f green:156/255.0f blue:225/255.0f alpha:1.0]];
     [registerButton setBackgroundImage:[UIImage imageNamed:@"icon"] forState:UIControlStateHighlighted];
     [registerButton setTitle:@"Register" forState:UIControlStateNormal];
     [registerButton setFrame:CGRectMake(self.view.frame.size.width - 113 - 10,CGRectGetMinY(loginButton.frame), 113, 41)];
     [self.view addSubview:registerButton];
+    
     [registerButton addTarget:self action:@selector(doRegister:) forControlEvents:UIControlEventTouchUpInside];
     [loginButton addTarget:self action:@selector(doLogin:) forControlEvents:UIControlEventTouchUpInside];
     
     UITapGestureRecognizer *gesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyBoard) ];
-    
     [self.view addGestureRecognizer:gesture];
     
     
@@ -72,7 +80,8 @@
     
 }
 
--(void)dismissKeyBoard{
+-(void)dismissKeyBoard
+{
     [self.emailField resignFirstResponder];
     [self.passwordField resignFirstResponder];
 }
